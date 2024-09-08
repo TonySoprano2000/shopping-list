f82a0465075a474efd5d679bd612fd5df468b32c
Bojan Mitrusic <b.mitrusic2000@gmail.com>
2024-09-08 17:40:53 +0200

    Filter is added on application

--- script.js
+++ script.js
@@ -65,6 +65,21 @@ function clearItems() {
   checkUI();
 }
 
+function FilterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    for (const letter of text) {
+      if (itemName.includes(letter)) {
+        item.style.display = "flex";
+      } else {
+        item.style.display = "none";
+      }
+    }
+  });
+}
+
 function checkUI() {
   const items = itemList.querySelectorAll("li");
   if (items.length === 0) {
@@ -79,5 +94,6 @@ function checkUI() {
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", FilterItems);
 
 checkUI();
