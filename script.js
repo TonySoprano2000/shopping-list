c12deae97dea224199717f1c5c43196cac5e5bd1
Bojan Mitrusic <b.mitrusic2000@gmail.com>
2024-09-12 21:07:12 +0200

    Local Storage is added

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
 const itemForm = document.getElementById("item-form");
-const itemList = document.getElementById("item-list");
 const itemInput = document.getElementById("item-input");
-const itemFilter = document.getElementById("filter");
+const itemList = document.getElementById("item-list");
 const clearBtn = document.getElementById("clear");
+
+const filter = document.getElementById("filter");
 
 function onAddItemSubmit(e) {
   e.preventDefault();
   const newItem = itemInput.value;
   if (newItem === "") {
-    // ne zaboravi .value zato sto mora da se unese vrednost a vrednost ne moze biti prazna tako da je to ovaj uslov.s
     alert("Please add item");
     return;
   }
-  // Create Item DOM element
-  addItemToDOM(newItem);
-  // Add item to local storage
+  // Create item DOM element
+  addItemToDom(newItem);
+  //
+  // Add item to local storage.
   addItemToStorage(newItem);
+  function addItemToDom(item) {
+    // Kreiraj ListItem
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(item));
 
-  // Check Ui
-  checkUI();
-  itemInput.value = "";
-}
+    // Kreiraj dugme za uklanjanje
+    const button = createButton("remove-item btn-link text-red");
+    li.appendChild(button);
 
-function addItemToStorage(item) {
-  let itemFromStorage;
-
-  if (localStorage.getItem("items") === null) {
-    itemFromStorage = [];
-  } else {
-    itemFromStorage = JSON.parse(localStorage.getItem("items"));
+    // Dodaj li na listu
+    itemList.appendChild(li);
   }
-  // Add new item to array
-  itemFromStorage.push(item);
-  //Convert to the JSON string and set to local storage
-  localStorage.setItem("item", JSON.stringify(itemFromStorage));
-}
 
-function addItemToDOM(item) {
-  // Create ListItem
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(item));
-  const button = createButton("remove-item btn-link text-red"); // Ovo je classes i to prosledjujes u funkciju
-  // Icon vezujes za button u funkciji gde pravis dugme
-  li.appendChild(button);
-  // Add li to the DOM
-  itemList.appendChild(li);
+  function addItemToStorage(item) {
+    let itemsFromStorage;
+    if (localStorage.getItem("items") === null) {
+      itemsFromStorage = [];
+    } else {
+      itemsFromStorage = JSON.parse(localStorage.getItem("items"));
+    }
+    itemsFromStorage.push(item);
+
+    // Convert to JSON string and set to local storage
+    localStorage.setItem("items", JSON.stringify(itemsFromStorage));
+  }
+  function getItemsFromStorage() {}
+
+  // Očisti unos
+  itemInput.value = "";
+  checkUI();
 }
 
 function createButton(classes) {
@@ -62,63 +64,51 @@ function createIcon(classes) {
 }
 
 function removeItem(e) {
+  // Proverava da li je element na koji je kliknuto (e.target) unutar elementa sa klasom "remove-item"
   if (e.target.parentElement.classList.contains("remove-item")) {
+    // Ako jeste, uklanja roditeljski element roditeljskog elementa (što je <li> element) sa liste
     if (confirm("Are you sure?")) {
       e.target.parentElement.parentElement.remove();
-
       checkUI();
     }
-
-    /*  Provera ciljanog elementa (e.target):
-
-e.target je element na koji je korisnik kliknuo.
-e.target.parentElement je roditeljski element ciljanog elementa.
-Provera klase roditeljskog elementa:
-classList.contains("remove-item") proverava da li roditeljski element ima klasu "remove-item".
-Ako roditeljski element ima ovu klasu, to znači da je kliknuto na dugme za uklanjanje.
-Uklanjanje stavke iz liste:
-e.target.parentElement.parentElement.remove() uklanja dedu ciljanog elementa (što je <li> element) iz DOM-a.
-Ovo efektivno uklanja stavku iz liste. */
   }
 }
 
-function clearItems() {
+function clearAllItems(e) {
+  // Postavlja unutrašnji HTML elementa itemList na prazan string, što uklanja sve stavke sa liste
   itemList.innerHTML = "";
   checkUI();
-}
-
-function FilterItems(e) {
-  const items = itemList.querySelectorAll("li"); // Pronalazi sve <li> elemente u listi
-  const text = e.target.value.toLowerCase(); // Uzima vrednost iz input polja i pretvara je u mala slova
-  items.forEach((item) => {
-    // Iterira kroz svaki <li> element
-    const itemName = item.firstChild.textContent.toLowerCase(); // Uzima tekst iz <li> elementa i pretvara ga u mala slova
-    for (const letter of text) {
-      // Iterira kroz svaki karakter u tekstu iz input polja
-      if (itemName.includes(letter)) {
-        // Proverava da li ime stavke sadrži karakter iz input polja
-        item.style.display = "flex"; // Ako sadrži, prikazuje stavku
-      } else {
-        item.style.display = "none"; // Ako ne sadrži, sakriva stavku
-      }
-    }
-  });
 }
 
 function checkUI() {
   const items = itemList.querySelectorAll("li");
   if (items.length === 0) {
     clearBtn.style.display = "none";
-    itemFilter.style.display = "none";
+    filter.style.display = "none";
   } else {
     clearBtn.style.display = "block";
-    itemFilter.style.display = "block";
+    filter.style.display = "block";
   }
 }
-// Event Listener
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+  /* Ova linija uzima vrednost unosa (tekst koji je korisnik upisao) i pretvara ga u mala slova pomoću .toLowerCase(). 
+  Razlog za to je da se obezbedi da pretraga nije osetljiva na velika/mala slova. */
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.includes(text)) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+// Dodaj event listener
 itemForm.addEventListener("submit", onAddItemSubmit);
 itemList.addEventListener("click", removeItem);
-clearBtn.addEventListener("click", clearItems);
-itemFilter.addEventListener("input", FilterItems);
-
+clearBtn.addEventListener("click", clearAllItems);
+filter.addEventListener("input", filterItems);
 checkUI();
