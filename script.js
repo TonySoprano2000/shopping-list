6f6fee1c594a49a99bdcfea889d38f601193496b
TonySoprano2000 <b.mitrusic2000@gmail.com>
2024-09-14 00:10:00 +0200

    Nesting of function is fixed

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const itemForm = document.getElementById("item-form");
-const itemInput = document.getElementById("item-input");
-const itemList = document.getElementById("item-list");
-const clearBtn = document.getElementById("clear");
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
-const filter = document.getElementById("filter");
-
+const filter = document.getElementById('filter');
+function displayItemsFromStorage() {
+  const itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage.forEach((item) => addItemToDom(item));
+  checkUI();
+}
 function onAddItemSubmit(e) {
   e.preventDefault();
   const newItem = itemInput.value;
-  if (newItem === "") {
-    alert("Please add item");
+  if (newItem === '') {
+    alert('Please add item');
     return;
   }
   // Create item DOM element
@@ -17,57 +21,55 @@ function onAddItemSubmit(e) {
   //
   // Add item to local storage.
   addItemToStorage(newItem);
-  function addItemToDom(item) {
-    // Kreiraj ListItem
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(item));
-
-    // Kreiraj dugme za uklanjanje
-    const button = createButton("remove-item btn-link text-red");
-    li.appendChild(button);
-
-    // Dodaj li na listu
-    itemList.appendChild(li);
-  }
-
-  function addItemToStorage(item) {
-    let itemsFromStorage;
-    if (localStorage.getItem("items") === null) {
-      itemsFromStorage = [];
-    } else {
-      itemsFromStorage = JSON.parse(localStorage.getItem("items"));
-    }
-    itemsFromStorage.push(item);
-
-    // Convert to JSON string and set to local storage
-    localStorage.setItem("items", JSON.stringify(itemsFromStorage));
-  }
-  function getItemsFromStorage() {}
-
-  // Očisti unos
-  itemInput.value = "";
+  itemInput.value = '';
   checkUI();
 }
+function addItemToDom(item) {
+  // Kreiraj ListItem
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
 
+  // Kreiraj dugme za uklanjanje
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+
+  // Dodaj li na listu
+  itemList.appendChild(li);
+}
 function createButton(classes) {
-  const button = document.createElement("button");
+  const button = document.createElement('button');
   button.className = classes;
-  const icon = createIcon("fa-solid fa-xmark");
+  const icon = createIcon('fa-solid fa-xmark');
   button.appendChild(icon);
   return button;
 }
-
 function createIcon(classes) {
-  const icon = document.createElement("i");
+  const icon = document.createElement('i');
   icon.className = classes;
   return icon;
+}
+function addItemToStorage(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage.push(item);
+
+  // Convert to JSON string and set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+function getItemsFromStorage() {
+  let itemsFromStorage;
+  if (localStorage.getItem('items') === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+  }
+  return itemsFromStorage;
 }
 
 function removeItem(e) {
   // Proverava da li je element na koji je kliknuto (e.target) unutar elementa sa klasom "remove-item"
-  if (e.target.parentElement.classList.contains("remove-item")) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
     // Ako jeste, uklanja roditeljski element roditeljskog elementa (što je <li> element) sa liste
-    if (confirm("Are you sure?")) {
+    if (confirm('Are you sure?')) {
       e.target.parentElement.parentElement.remove();
       checkUI();
     }
@@ -76,39 +78,43 @@ function removeItem(e) {
 
 function clearAllItems(e) {
   // Postavlja unutrašnji HTML elementa itemList na prazan string, što uklanja sve stavke sa liste
-  itemList.innerHTML = "";
+  itemList.innerHTML = '';
   checkUI();
 }
 
 function checkUI() {
-  const items = itemList.querySelectorAll("li");
+  const items = itemList.querySelectorAll('li');
   if (items.length === 0) {
-    clearBtn.style.display = "none";
-    filter.style.display = "none";
+    clearBtn.style.display = 'none';
+    filter.style.display = 'none';
   } else {
-    clearBtn.style.display = "block";
-    filter.style.display = "block";
+    clearBtn.style.display = 'block';
+    filter.style.display = 'block';
   }
 }
 
 function filterItems(e) {
-  const items = itemList.querySelectorAll("li");
+  const items = itemList.querySelectorAll('li');
   const text = e.target.value.toLowerCase();
   /* Ova linija uzima vrednost unosa (tekst koji je korisnik upisao) i pretvara ga u mala slova pomoću .toLowerCase(). 
   Razlog za to je da se obezbedi da pretraga nije osetljiva na velika/mala slova. */
   items.forEach((item) => {
     const itemName = item.firstChild.textContent.toLowerCase();
     if (itemName.includes(text)) {
-      item.style.display = "flex";
+      item.style.display = 'flex';
     } else {
-      item.style.display = "none";
+      item.style.display = 'none';
     }
   });
 }
-
-// Dodaj event listener
-itemForm.addEventListener("submit", onAddItemSubmit);
-itemList.addEventListener("click", removeItem);
-clearBtn.addEventListener("click", clearAllItems);
-filter.addEventListener("input", filterItems);
-checkUI();
+// Inicijalizacija aplikacije
+function init() {
+  // Dodaj event listener
+  itemForm.addEventListener('submit', onAddItemSubmit);
+  itemList.addEventListener('click', removeItem);
+  clearBtn.addEventListener('click', clearAllItems);
+  filter.addEventListener('input', filterItems);
+  document.addEventListener('DOMContentLoaded', displayItemsFromStorage);
+  checkUI();
+}
+init();
